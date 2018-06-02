63a7655e79006932328a4913cd69c2bf4296853a
Elton Moura <elton.ms@gmail.com>
2018-06-01 22:01:23 -0300

    Incluindo phpdocs de UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+/**
+ * Classe que implementa o CRUDL de Usuários
+ */
 class UserController extends Controller
 {
+    /**
+     * Cria um novo Usuário
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function create(Request $request)
     {
         $postData = $request->json()->all();
@@ -16,17 +25,32 @@ class UserController extends Controller
         $user->name = $postData['name'];
         $user->email = $postData['email'];
         $user->password = User::encrypt($postData['password']);
+
         $results = $user->save();
 
         return response()->json($results, 200);
     }
 
+    /**
+     * Retorna um Usuário identificado pelo seu Id
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return Response
+     */
     public function read(Request $request, $id)
     {
         $user = User::findOrFail($id);
         return response()->json($user, 200);
     }
 
+    /**
+     * Edita um Usuário identificado pelo seu Id
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return Response
+     */
     public function update(Request $request, $id)
     {
         $postData = $request->json()->all();
@@ -46,6 +70,13 @@ class UserController extends Controller
         return response()->json($user, 200);
     }
 
+    /**
+     * Remove um Usuário identificado pelo seu Id
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return Response
+     */
     public function delete(Request $request, $id)
     {
         $user = User::findOrFail($id);
@@ -53,6 +84,12 @@ class UserController extends Controller
         return response()->json($response, 200);
     }
 
+    /**
+     * Lista os Usuários existentes
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function list(Request $request)
     {
         return response()->json(User::all(), 200);
